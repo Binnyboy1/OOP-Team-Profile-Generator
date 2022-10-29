dd99c85db55085c1d4d347d38e03d3f54f1c1883
Binny <ibinstock@outlook.com>
2022-10-28 21:21:31 -0800

    finished page-template.js and added hyperlinks functionality

--- Main/src/page-template.js
+++ Main/src/page-template.js
@@ -2,8 +2,6 @@ const generateTeam = team => {
 
     // create the manager html
     const generateManager = manager => {
-        // console.log(Object.values(manager));
-        // console.log(Object.keys(manager));
         return `<div class="bg-indigo-100 m-4 w-auto w-64 h-auto shadow-xl">
                     <div class="bg-indigo-500 p-4">
                         <p class="text-white text-4xl">${manager.name}</p>
@@ -11,7 +9,7 @@ const generateTeam = team => {
                     </div>
                     <div class="p-6">
                         <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">ID: ${manager.id}</p>
-                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Email: ${manager.email}</p>
+                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                         <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Office number: ${manager.officeNumber}</p>
                     </div>
                 </div>`;
@@ -19,8 +17,6 @@ const generateTeam = team => {
 
     // create the html for engineers
     const generateEngineer = engineer => {
-        // console.log(Object.values(engineer));
-        // console.log(Object.keys(engineer));
         return `<div class="bg-indigo-100 m-4 w-auto w-64 h-auto shadow-xl">
                     <div class="bg-indigo-500 p-4">
                         <p class="text-white text-4xl">${engineer.name}</p>
@@ -28,16 +24,14 @@ const generateTeam = team => {
                     </div>
                     <div class="p-6">
                         <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">ID: ${engineer.id}</p>
-                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Email: ${engineer.email}</p>
-                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">GitHub: ${engineer.github}</p>
+                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
                     </div>
                 </div>`;
     };
 
     // create the html for interns
     const generateIntern = intern => {
-        // console.log(Object.values(intern));
-        // console.log(Object.keys(intern));
         return `<div class="bg-indigo-100 m-4 w-auto w-64 h-auto shadow-xl">
                     <div class="bg-indigo-500 p-4">
                         <p class="text-white text-4xl">${intern.name}</p>
@@ -45,7 +39,7 @@ const generateTeam = team => {
                     </div>
                     <div class="p-6">
                         <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">ID: ${intern.id}</p>
-                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Email: ${intern.email}</p>
+                        <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
                         <p class="bg-indigo-200 text-indigo-900 text-base p-2 m-1">School: ${intern.school}</p>
                     </div>
                 </div>`;
@@ -57,13 +51,10 @@ const generateTeam = team => {
         // template: generate<NAME>( <DATA> );
         globalHTML += eval(`generate${employee.constructor.name}`)(employee);
     });
-    // console.log(globalHTML);
 
     return globalHTML;
 }
 
-// module.exports = generateTeam;
-// /*
 module.exports = team => {
     // template literal - html body that calls the generate team function
     return `<!DOCTYPE html>
@@ -100,4 +91,3 @@ module.exports = team => {
     </html>
     `
 }
-// */
